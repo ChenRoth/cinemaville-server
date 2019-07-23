6336439bdec201a412c8a8292980798f59b68ff5
noUser <noUser>
2019-07-23 20:23:11 +0300

    select specific fields in GET /movies

--- routes/movies.js
+++ routes/movies.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const { Movie } = require('../models/Movie');
 
 router.get('/', async function (req, res, next) {
-    const movies = await Movie.find().exec();
+    const movies = await Movie.find().select({ name: 1, year: 1, genre: 1, image: 1 }).exec();
     res.send(movies);
 });
 
