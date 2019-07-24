182efa98b3509174f74ae017cca43fa7efdbba43
Chen Roth <rothchen@gmail.com>
2019-07-24 12:25:03 +0300

    add POST /movies endpoint with dynamic genre server validation

--- models/Movie.js
+++ models/Movie.js
@@ -1,12 +1,24 @@
 const mongoose = require('mongoose');
+const {Genre} = require('./Genre');
 
 const movieSchema = mongoose.Schema({
-    name: String,
-    genre: String,
-    actors: [String],
-    year: { type: Number, min: 1850 },
-    image: String,
-    video: String,
+    name: {type: String, required: true},
+    genre: {
+        type: String,
+        required: true,
+        validate: {
+            async validator(genreName) {
+                const genres = await Genre.find().exec();
+                return genres.some(genre => genre.name === genreName);
+
+            },
+            message: props => `${props.value} is an unknown genre`,
+        }
+    },
+    actors: {type: [String], required: true},
+    year: {type: Number, min: 1850, required: true},
+    image: {type: String, required: true},
+    video: {type: String, required: true}
 });
 
 const Movie = mongoose.model('movie', movieSchema);
--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const { Movie } = require('../models/Movie');
+const { Genre } = require('../models/Genre');
 
 router.get('/', async function (req, res, next) {
     const movies = await Movie.find().select({ name: 1, year: 1, genre: 1, image: 1 }).exec();
@@ -17,5 +18,16 @@ router.get('/:id', async function (req, res, next) {
     }
 });
 
+
+router.post('/', async (req, res) => {
+   const {name, year, actors, genre, image, video} = req.body;
+   const movie = new Movie({name, year, actors, genre, image, video});
+   try {
+       const document = await movie.save();
+       res.status(200).send(document);
+   } catch (e) {
+        res.status(400).send(e);
+   }
+});
 
 module.exports = router;
